013f47840336cee7d71dce3b2fded15f50df5545
김민정 <kimminjeung@wizzpay.co.kr>
2022-04-15 16:28:03 +0900

    Add Error Bubbling

--- 3.error-bubbling.js
+++ 3.error-bubbling.js
@@ -1,4 +1,5 @@
 // Bubbling up, Propagating
+// - 에러를 처리하기에 적당한 곳에서 에러를 처리한다.
 
 function a() {
   throw new Error('error!');
@@ -8,7 +9,7 @@ function b() {
   try {
     a();
   } catch (error) {
-    console.log(error);
+    console.log('error ');
   }
 }
 
